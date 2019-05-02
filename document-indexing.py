789cbc206c6952986f835fd4bcedc90ee06fed9f
es2812 <es2812@hotmail.com>
2019-05-02 16:47:29 +0200

    Fixed overlapping indexes in norte dataset by removing manual indexing

--- document-indexing.py
+++ document-indexing.py
@@ -3,7 +3,7 @@
     de Elasticsearch (localhost:9200) para el indexado de los conjuntos de datos
     `inn`, `marathon`, `norte` y `wine`.
 
-    Dependencias: requests
+    Dependencias: requests, json, os, re (los últimos tres son librerías por defecto en Python3)
 """
 
 import json
@@ -11,7 +11,7 @@ import requests
 import os
 import re
 
-ELASTIC = "http://localhost:9200"
+ELASTIC = "http://localhost:9200" #dirección en la que está funcionando elastic
 
 path_script = os.path.dirname(os.path.realpath(__file__)) #permite acceder a los ficheros en path relativo al script
 
@@ -27,44 +27,58 @@ def createIndex(elastic_url,index_name,mapp):
         # 400 significa error. El índice no se ha creado, normalmente porque ya existía. Eliminamos el índice.
         res = requests.delete(elastic_url+"/"+index_name)
         res = requests.put(elastic_url+"/"+index_name, json={"mappings":mapp})
-        print(res.text)
 
     if(res.status_code==200):
         print("Index %s creado" % index_name)
+        return
     else:
         print("Error en creación de index %s" % index_name)
+        return
 
 def uploadBulk(abs_path, elastic_url, index_name):
     """
-    Realiza la subida de los ficheros encontrados en abs_path al índice con nombre index_name en elastic corriendo en elastic_url
+    Realiza la subida de los ficheros encontrados en abs_path al índice con nombre index_name en elastic corriendo en elastic_url. 
+    También realiza algunas modificaciones en inn y maraton (debido al formato de fechas/tiempos).
+
+    Si ya existen documentos en el índice los añade a continuación.
     """
-
     with open(abs_path) as f:
-        obj = json.load(f)
+        obj = json.load(f) #leemos los datos a una lista de diccionarios
 
-    #construimos los datos a enviar para el _bulk
+    # Construimos los datos a enviar.
+    # El formato debe ser, para cada instancia:
+    #   - una línea {"index": {} } (con índice en blanco elasticsearch automáticamente crea uno único)
+    #   - seguida de una línea con el contenido del documento
     data = ''
 
-    for index, doc in enumerate(obj):
-        data += '{ "index": {"_id": '+str(index)+'}}\n' #añadimos el índice del documento
-        data += json.dumps(doc).replace("},","}")
-        data += '\n'
-    data += '\n'
+    for doc in obj:
+        data += '{ "index": {}}\n'
+        data += json.dumps(doc)+'\n'
 
-    # En el fichero "inn.json" las fechas se encuentran escritas con el mes en mayúsuclas (p.e. 02-DEC-17). el DateFormatter de Java no puede leer esto. 
-    # Utilizamos regex para pasar las últimas letras del mes a minúsculas en estos casos
+    # En el fichero "inn.json" las fechas se encuentran escritas con el mes en mayúsuclas (p.e. 02-DEC-17). 
+    # El DateFormatter de Java no puede leer ese mes correctamente.
+    # Utilizamos regex para detectar estos casos y pasar las últimas letras del mes a minúsculas.
     data = re.sub(r'(\d{2})-([A-Z]{3})-(\d{2})',lambda m: m.group(1)+"-"+m.group(2)[0]+m.group(2)[1:].lower()+"-"+m.group(3),data)
 
-    # En el fichero "maraton.json" el campo Pace se encuentra en minutos y segundos, para poder almacenarlo como fecha se debe añadir la hora delante (0 en este caso)
+    # En el fichero "maraton.json" el campo Pace se encuentra en minutos y segundos, 
+    # para poder almacenarlo como fecha se debe añadir la hora delante (0 en este caso)
     data = re.sub(r'"(\d+:\d{2}")',lambda m: '"0:'+m.group(1),data)
 
+    # Subida de los documentos a elastic
     res = requests.post(elastic_url+"/"+index_name+"/_bulk", headers={'Content-Type': 'application/x-ndjson'}, data=data)
 
     if(res.status_code==200):
         print("%d documentos almacenados a index %s" % (len(obj),index_name))
+        return
     else:
         print("Error en almacenamiento de documentos a index %s" % index_name)
+        print(res.text)
+        return
 
+
+"""
+            WINE DATASET
+"""
 pathwine = os.path.join(path_script,"data/wine/wine.json")
 
 mapwine = { "properties" : {
@@ -77,6 +91,10 @@ mapwine = { "properties" : {
 
 createIndex(ELASTIC,"wine",mapwine)
 uploadBulk(pathwine,ELASTIC,"wine")
+
+"""
+            INN DATASET
+"""
 
 pathinn = os.path.join(path_script,"data/inn/inn.json")
 
@@ -100,6 +118,10 @@ mapinn = { "properties" : {
 createIndex(ELASTIC,"inn",mapinn)
 uploadBulk(pathinn,ELASTIC,"inn")
 
+"""
+            MARATON DATASET
+"""
+
 pathmar = os.path.join(path_script,"data/maraton/maraton.json")
 
 mapmar = { "properties" : {
@@ -117,17 +139,23 @@ mapmar = { "properties" : {
 createIndex(ELASTIC,"maraton",mapmar)
 uploadBulk(pathmar,ELASTIC,"maraton")
 
-pathrootnorte = os.path.join(path_script,"data/norte/")
+# """
+#             NORTE DATASET
+# """
 
-mapnorte = { "properties" : {
-    "docid" : { "type" : "long" },
-    "fecha" : { "type" : "date",
-                  "format" : "dd-mm-yyyy"}
-    }
-}
+# pathrootnorte = os.path.join(path_script,"data/norte/")
 
-createIndex(ELASTIC,"norte",mapnorte)
+# mapnorte = { "properties" : {
+#     "docid" : { "type" : "long" },
+#     "fecha" : { "type" : "date",
+#                   "format" : "dd-mm-yyyy"}
+#     }
+# }
 
-for dir,_,files in os.walk(pathrootnorte):
-    for file in files:
-        uploadBulk(os.path.join(dir,file),ELASTIC,"norte")
+# createIndex(ELASTIC,"norte",mapnorte)
+
+# for dir,_,files in os.walk(pathrootnorte): 
+#     #los ficheros se encuentran en múltiples directorios, los recorremos todos y lanzamos un bulk 
+#     # por cada fichero json encontrados
+#     for file in files:
+#         uploadBulk(os.path.join(dir,file),ELASTIC,"norte")
